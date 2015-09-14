696019aa864b5eb621ced99ec116b02a5db9dbab
amamama <amtkrogzo@yahoo.co.jp>
2015-09-14 15:55:41 +0900

    Update m.cpp

--- m.cpp
+++ m.cpp
@@ -246,7 +246,7 @@ namespace USART{
 		USART_SendData(USART2, data);
 	}
 
-	void outputString(char* str){
+	void outputString(const char* str){
 		while (*str)
 		{
 			while (USART_GetFlagStatus(USART2, USART_FLAG_TXE) == RESET); // Wait for Empty
@@ -260,7 +260,7 @@ namespace USART{
 		Stream(){}
 		~Stream(){}
 
-		Stream& operator<<(char* s){
+		Stream& operator<<(const char* s){
 			outputString(s);
 			return *this;
 		}
@@ -462,59 +462,98 @@ namespace Timer{
 namespace STEP{
 
 	//SPI1 module for moter driving
-	GPIO::AssignmentHolder stp1_clk(GPIOA, GPIO_Pin_7);//CN10.15
-	GPIO::AssignmentHolder stp1_dir(GPIOA, GPIO_Pin_11);//CN10.14
-	GPIO::AssignmentHolder stp1_rst(GPIOA, GPIO_Pin_12);//CN10.12
+	GPIO::AssignmentHolder stpm_clk(GPIOA, GPIO_Pin_7);//CN10.15
+	GPIO::AssignmentHolder stpm_dir(GPIOA, GPIO_Pin_11);//CN10.14
+	GPIO::AssignmentHolder stpm_rst(GPIOA, GPIO_Pin_12);//CN10.12
 
-	volatile unsigned long stp1_cnt = 0;
+	GPIO::AssignmentHolder stpa_clk(GPIOA, GPIO_Pin_0);//CN7.28
+	GPIO::AssignmentHolder stpa_dir(GPIOA, GPIO_Pin_1);//CN7.30
+	GPIO::AssignmentHolder stpa_rst(GPIOA, GPIO_Pin_4);//CN7.32
+
+	volatile unsigned long stpm_cnt = 0;
+	volatile unsigned long stpa_cnt = 0;
 	typedef enum {
 		CW,		//Set
 		CCW,	//Reset.tabun
 	} dir_e;
-	dir_e nowdir = CCW;
+	dir_e nowdir_m = CCW, nowdir_a = CCW;
 
 	void setting(void){
 		GPIO_InitTypeDef GPIO_InitStructure = {};
 
-		GPIO_InitStructure.GPIO_Pin = stp1_clk.getPin() | stp1_dir.getPin() | stp1_rst.getPin();
+		GPIO_InitStructure.GPIO_Pin = stpm_clk.getPin() | stpm_dir.getPin() | stpm_rst.getPin()//;
+									| stpa_clk.getPin() | stpa_dir.getPin() | stpa_rst.getPin();
 		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
 		GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 		GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 		//GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
-		GPIO_Init(stp1_clk.getGPIO(), &GPIO_InitStructure);
+		GPIO_Init(stpm_clk.getGPIO(), &GPIO_InitStructure);
 
 	}
 	
-	void reset_stp1(void) {
-		IO::Reset(stp1_rst);
-		IO::Reset(stp1_dir);
-		IO::Reset(stp1_clk);
+	void reset_stpm(void) {
+		IO::Reset(stpm_rst);
+		IO::Reset(stpm_dir);
+		IO::Reset(stpm_clk);
 		Delay(0xffff);//1ms待ちたい
-		IO::Set(stp1_rst);
+		IO::Set(stpm_rst);
 		Delay(0xffff);//1ms待ちたい
-		IO::Reset(stp1_rst);
+		IO::Reset(stpm_rst);
+		Delay(0xffff);//1ms待ちたい
+	}
+
+	void reset_stpa(void) {
+		IO::Reset(stpa_rst);
+		IO::Reset(stpa_dir);
+		IO::Reset(stpa_clk);
+		Delay(0xffff);//1ms待ちたい
+		IO::Set(stpa_rst);
+		Delay(0xffff);//1ms待ちたい
+		IO::Reset(stpa_rst);
 		Delay(0xffff);//1ms待ちたい
 	}
 
 	void reset_stp(void) {
-		reset_stp1();
+		reset_stpm();
+		Delay(0xffff);//1ms待ちたい
+		reset_stpa();
+		Delay(0xffff);//1ms待ちたい
 	}
 
-	void stp1Pulse(dir_e dir){
-		if(dir == CW  && nowdir != CW ) {
-			IO::Set(stp1_dir);
-			nowdir = CW;
-		} else if(dir == CCW && nowdir != CCW) {
-			IO::Reset(stp1_dir);
-			nowdir = CCW;
+	void stpm_Pulse(dir_e dir){
+		if(dir == CW  && nowdir_m != CW ) {
+			IO::Set(stpm_dir);
+			nowdir_m = CW;
+		} else if(dir == CCW && nowdir_m != CCW) {
+			IO::Reset(stpm_dir);
+			nowdir_m = CCW;
 		}
 		static bool p = true;
 		if (p){
-			IO::Reset(stp1_clk);
-			stp1_cnt++;
+			IO::Reset(stpm_clk);
+			stpm_cnt++;
 		}
 		else{
-			IO::Set(stp1_clk);
+			IO::Set(stpm_clk);
+		}
+		p = !p;
+	}
+
+	void stpa_Pulse(dir_e dir){
+		if(dir == CW  && nowdir_m != CW ) {
+			IO::Set(stpa_dir);
+			nowdir_a = CW;
+		} else if(dir == CCW && nowdir_a != CCW) {
+			IO::Reset(stpa_dir);
+			nowdir_a = CCW;
+		}
+		static bool p = true;
+		if (p){
+			IO::Reset(stpa_clk);
+			stpm_cnt++;
+		}
+		else{
+			IO::Set(stpa_clk);
 		}
 		p = !p;
 	}
@@ -524,10 +563,9 @@ namespace STEP{
 namespace PWM{
 
 	GPIO::AssignmentHolder pwm_1(GPIOB, GPIO_Pin_6);//CN10.17
-	GPIO::AssignmentHolder pwm_3(GPIOB, GPIO_Pin_8);//CN10.4
+	GPIO::AssignmentHolder pwm_2(GPIOB, GPIO_Pin_7);//CN7.21 //F
+	GPIO::AssignmentHolder pwm_3(GPIOB, GPIO_Pin_8);//CN10.3
 	GPIO::AssignmentHolder pwm_4(GPIOB, GPIO_Pin_9);//CN10.5
-	//GPIO::AssignmentHolder stp1_dir(GPIOA, GPIO_Pin_11);//CN10.14
-	//GPIO::AssignmentHolder stp1_rst(GPIOA, GPIO_Pin_12);//CN10.12
 	
 	const unsigned short period = 1000;
 	const unsigned short prescale = 84;
@@ -535,7 +573,7 @@ namespace PWM{
 
 	void setting(void){
 		GPIO_InitTypeDef GPIO_InitStructure = {0};
-		GPIO_InitStructure.GPIO_Pin = pwm_1.getPin() | pwm_3.getPin()| pwm_4.getPin();
+		GPIO_InitStructure.GPIO_Pin = pwm_1.getPin() | pwm_2.getPin() | pwm_3.getPin()| pwm_4.getPin();
 		GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
 		GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
 		GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
@@ -543,6 +581,7 @@ namespace PWM{
 		GPIO_Init(pwm_1.getGPIO(), &GPIO_InitStructure);
 
 		GPIO_PinAFConfig(pwm_1.getGPIO(), pwm_1.getPinSource(), GPIO_AF_TIM4);
+		GPIO_PinAFConfig(pwm_2.getGPIO(), pwm_2.getPinSource(), GPIO_AF_TIM4);
 		GPIO_PinAFConfig(pwm_3.getGPIO(), pwm_3.getPinSource(), GPIO_AF_TIM4);
 		GPIO_PinAFConfig(pwm_4.getGPIO(), pwm_4.getPinSource(), GPIO_AF_TIM4);
 		
@@ -566,6 +605,8 @@ namespace PWM{
 		
 		TIM_OC1Init(TIM4, &TIM_OCInitStructure);
 		TIM_OC1PreloadConfig(TIM4, TIM_OCPreload_Enable);
+		TIM_OC2Init(TIM4, &TIM_OCInitStructure);
+		TIM_OC2PreloadConfig(TIM4, TIM_OCPreload_Enable);
 		TIM_OC3Init(TIM4, &TIM_OCInitStructure);
 		TIM_OC3PreloadConfig(TIM4, TIM_OCPreload_Enable);
 		TIM_OC4Init(TIM4, &TIM_OCInitStructure);
@@ -581,6 +622,11 @@ namespace PWM{
 		TIM_SetCompare1(TIM4, (unsigned long)(ratio*Hz));
 	}
 
+	void duty2(float ratio){
+		if (ratio < 0 || ratio > 1) return;
+		TIM_SetCompare2(TIM4, (unsigned long)(ratio*Hz));
+	}
+
 	void duty3(float ratio){
 		if (ratio < 0 || ratio > 1) return;
 		TIM_SetCompare3(TIM4, (unsigned long)(ratio*Hz));
@@ -589,6 +635,29 @@ namespace PWM{
 	void duty4(float ratio){
 		if (ratio < 0 || ratio > 1) return;
 		TIM_SetCompare4(TIM4, (unsigned long)(ratio*Hz));
+	}
+
+	void duty(int n, float ratio) {
+		switch (n) {
+			case 1:
+			duty1(ratio);
+			break;
+
+			case 2:
+			duty2(ratio);
+			break;
+
+			case 3:
+			duty3(ratio);
+			break;
+
+			case 4:
+			duty4(ratio);
+			break;
+
+			default:
+			break;
+		}
 	}
 }
 
@@ -704,6 +773,15 @@ volatile char motor_error_flag = 0;	//モータ関係のエラーコード
 volatile char motorL_duty = motorL_duty_initial;	//今送るモーター速度(左)
 volatile char motorR_duty = motorR_duty_initial;	//(右)
 
+volatile unsigned char ball = 15;
+STEP::dir_e stpa_dir = STEP::CCW;
+volatile unsigned int magazine_move = 0;
+volatile unsigned int phase = 0;
+/*
+OOO__ _OOO_ __OOO
+p0    p1    p2
+*/
+
 
 PS2Controller c;
 int main(void)
@@ -741,11 +819,15 @@ int main(void)
 	Timer::begin2();//モータ用割り込み発生開始
 	Timer::begin3();//モータ用割り込み発生開始
 	STEP::reset_stp();
-	PWM::duty1(0.0f);
 
 	while (1){
 		//USART::out << (uint8_t)c.get_button(0) << "," << (uint8_t)c.get_button(1) <<" " << "Lv:" << c.get_stick(1, 1) << "Ls:" << c.get_stick(1, 0) << "Rv:" << c.get_stick(0, 1) << "Rs:" << c.get_stick(0, 0) << USART::endl;
-		if(!IO::Read(IO::micro_start)) USART::out << "pushed" << USART::endl;//micro_switch 押されてないと1,押されると0
+		//if(!IO::Read(IO::micro_start)) USART::out << "pushed" << USART::endl;//micro_switch 押されてないと1,押されると0
+		if(!ball) USART::out << "empty" << USART::endl;
+		if(ball == 10 || ball == 5) {
+			//magazine_move = 200;
+			USART::out << "5ko" << USART::endl;
+		}
 		Delay(10000);
 	}
 	
@@ -771,13 +853,43 @@ void motor_control(void){
 		dutyR = clamp_duty(motorR_duty - Kp_motorR_lineL*(sensor[1] - lineL_target) - Kp_motorR_lineR*(sensor[3] - lineL_target));
 	}*/
 
-	if(c.pushed(up)) {
+	/*if(c.pushed(up)) {
 		dutyR = motorR_duty;
 		dutyL = motorL_duty;
+	} else if (c.pushed(right)) {
+		dutyR = -motorR_duty;
+		dutyL = motorL_duty;
+	} else if (c.pushed(left)) {
+		dutyR = motorR_duty;
+		dutyL = -motorL_duty;
 	} else if (c.pushed(down)) {
 		dutyR = -motorR_duty;
 		dutyL = -motorL_duty;
+	} else {
+		dutyR = 0;
+		dutyL = 0;
+	}*/
+	if(c.pushed(up)) {
+		dutyR = 50;
+		dutyL = 50;
+	} else if (c.pushed(right)) {
+		dutyR = -50;
+		dutyL = 50;
+	} else if (c.pushed(left)) {
+		dutyR = 50;
+		dutyL = -50;
+	} else if (c.pushed(down)) {
+		dutyR = -50;
+		dutyL = -50;
+	} else {
+		dutyR = 0;
+		dutyL = 0;
 	}
+	/*if(c.pushed(up)) {
+		dutyR = -30;
+		dutyL = 30;
+	}*/
+
 
 	//データ転送
 	char datR = (char)SPI::transaction2((uint8_t)dutyR);
@@ -802,27 +914,83 @@ void motor_control(void){
 
 void step_control(void){
 	if(c.pushed(tri)) {
-		STEP::stp1Pulse(STEP::CCW);
+		STEP::stpm_Pulse(STEP::CCW);
 	} else if (c.pushed(cr)) {
-		STEP::stp1Pulse(STEP::CW);
+		STEP::stpm_Pulse(STEP::CW);
+	}
+
+	if(magazine_move) {
+		STEP::stpa_Pulse(stpa_dir);
+		magazine_move--;
 	}
 
 }
 
 void sya_control(void){
-	if(c.pushed(cir)) {
-		PWM::duty3(0.5f);
-		PWM::duty4(0.5f);
-	} else {
-		PWM::duty3(0.0f);
-		PWM::duty4(0.0f);
+	volatile static int rel = 0;
+	volatile static int ign_ham = 0;
+	volatile static int time = -1;
+	volatile static int isrot = 0;
+	if(rel) {
+		//USART::out << __LINE__ << USART::endl;//ありえない
+		PWM::duty(1, 0.8f);
+		PWM::duty(2, 0.0f);
 	}
-	if(c.triggered(cir) == 1) {
-		USART::out << "tri1" << USART::endl;//ありえない
+	if (IO::Read(IO::micro_end)) {
+		//USART::out << __LINE__ << USART::endl;//ありえない
+		PWM::duty(1, 0.0f);
+		PWM::duty(2, 0.8f);
+		rel = 0;
 	}
-	if(c.triggered(cir) == -1) {
-		USART::out << "tri-1" << USART::endl;//ありえない
+	if(!IO::Read(IO::micro_start) && ign_ham == 1 && time == -1) {
+		//USART::out << __LINE__ << USART::endl;//ありえない
+		PWM::duty(1, 0.0f);
+		PWM::duty(2, 0.0f);
+		time = 20;
+	}
+	if(time > 0) {
+		//USART::out << __LINE__ << USART::endl;//ありえない
+		PWM::duty(1, 0.0f);
+		PWM::duty(2, 0.5f);
+		time--;
+	} else if(time == 0) {
+		//USART::out << __LINE__ << USART::endl;//ありえない
+		PWM::duty(1, 0.0f);
+		PWM::duty(2, 0.0f);
+		rel = 0;
+		ign_ham = 0;
+		time = -1;
 
+		//PWM::duty(3, 0.0f);
+		//PWM::duty(4, 0.0f);
+	}
+	if(c.triggered(sq) == 1 && isrot == 0) {
+		//USART::out << __LINE__ << USART::endl;//ありえない
+		PWM::duty(3, 0.5f);
+		PWM::duty(4, 0.5f);
+		isrot = 1;
+	} else if (c.triggered(sq) == 1 && isrot == 1) {
+		//USART::out << __LINE__ << USART::endl;//ありえない
+		PWM::duty(3, 0.0f);
+		PWM::duty(4, 0.0f);
+		isrot = 0;
+	}
+	if(c.triggered(cir) == 1 && ign_ham == 0) {
+		rel = 1;
+		ign_ham = 1;
+
+		if (ball) ball--;
+		//USART::out << __LINE__<<","<<ball << USART::endl;//ありえない
+	}
+
+	if(c.triggered(r1) == 1 && (phase ==  0 || phase == 1) && magazine_move == 0 && ign_ham == 0) {
+		magazine_move = 475;
+		phase++;
+		stpa_dir = STEP::CCW;
+	} else if(c.triggered(l1) == 1 && (phase == 1 || phase == 2) && magazine_move == 0 && ign_ham == 0) {
+		magazine_move = 475;
+		phase--;
+		stpa_dir = STEP::CW;
 	}
 
 }
@@ -839,9 +1007,10 @@ extern "C" void TIM2_IRQHandler(void)
 		TIM_ClearITPendingBit(TIM2, TIM_IT_Update);
 
 		//USART::out << "e" << USART::endl;//ありえない
-		c.update(); 
+		c.update();
 		motor_control();
 		sya_control();
+		//step_control();
 	}
 }
 
@@ -849,6 +1018,7 @@ extern "C" void TIM2_IRQHandler(void)
 //モーターの速度調整
 extern "C" void TIM3_IRQHandler(void)
 {
+	volatile static int cnt = 0;
 	// グローバル割り込みから個別要因へジャンプするためにフラグチェックする
 	// 今回は要因が1つしかないけど、通常はこういうスタイルで分岐する
 	if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET)
@@ -857,7 +1027,9 @@ extern "C" void TIM3_IRQHandler(void)
 		TIM_ClearITPendingBit(TIM3, TIM_IT_Update);
 
 		//USART::out << "r" << USART::endl;//ありえない
-		step_control();
+		if (!(cnt%5)) step_control();
+		cnt++;
+		if (cnt == 100) cnt = 0;
 	}
 }
 /**************************************************************************************/
